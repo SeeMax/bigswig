374b40e49e4e867c5bbb8211e1fbd0cc80256321
Max Mearsheimer <mmearshe@gmail.com>
2014-04-17 12:29:15 -0500

    blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BigSwig::Application.config.secret_key_base = '09bf5a406cd17a1e0aa4834f1039c3ab780097d34764d2d769c5ad619db17ee0dbad89f47865ecdeadc1063ec3cf0f2038587fa2c1b7a886b824e8c6ab867cc1'
