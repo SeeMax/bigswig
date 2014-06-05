1c9d846e8c1894cdc09196be9f1e811b5e58837e
Max Mearsheimer <mmearshe@gmail.com>
2014-06-05 17:54:02 -0500

    nav scroll and way poiints working slightly improved

--- app/assets/javascripts/header_shrink.js
+++ app/assets/javascripts/header_shrink.js
@@ -2,11 +2,16 @@ $( document ).ready(function() {
   $(window).scroll(function () {
       if ($(document).scrollTop() <= 20) {
           $('.nav-bar').removeClass('small-nav', 1000);
-          $('.nav-right').css("margin-left" , "30%");
       } else {
           $('.nav-bar').addClass('small-nav', 1000);
-          $('.nav-right').css("margin-left" , "0%");
       }
+  });
 
+  $(window).scroll(function () {
+      if ($(document).scrollTop() <= 19) {
+        $('.nav-right').css("margin-left" , "30%");
+      } else {
+        $('.nav-right').css("margin-left" , "0%");
+      }
   });
 });
