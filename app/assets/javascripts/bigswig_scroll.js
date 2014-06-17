a6063106ea8d9f22892b513304339e5cfe6cdb8b
Max Mearsheimer <mmearshe@gmail.com>
2014-06-17 10:33:56 -0500

    disable skrollr mobile

--- app/assets/javascripts/bigswig_scroll.js
+++ app/assets/javascripts/bigswig_scroll.js
@@ -31,6 +31,12 @@ $(window).load(function() {
 
   });
 
+  if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+      skrollr.init({
+          forceHeight: false
+      });
+  }
+
 
   } )( jQuery );
 });
