8993b9a13062951d607419d7b96682b28eda8346
Max Mearsheimer <mmearshe@gmail.com>
2014-06-20 10:03:01 -0600

    updated header shrink

--- app/assets/javascripts/header_shrink.js
+++ app/assets/javascripts/header_shrink.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
   $(window).scroll(function () {
       if ($(document).scrollTop() <= 20) {
-          $('.nav-bar').removeClass('small-nav', 1000);
           $('.mobile-logo').removeClass('small-logo', 1000);
+          $('.nav-bar').removeClass('small-nav', 1000);
       } else {
-          $('.nav-bar').addClass('small-nav', 1000);
           $('.mobile-logo').addClass('small-logo', 1000);
+          $('.nav-bar').addClass('small-nav', 1000);
       }
   });
 
