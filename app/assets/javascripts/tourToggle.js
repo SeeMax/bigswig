b75ecce2dc6d3c94d75bf2d001f2f2c2099695de
Max Mearsheimer <max@seemaxwork.com>
2015-11-16 14:09:48 -0600

    updated copy and clickable areas

--- app/assets/javascripts/tourToggle.js
+++ app/assets/javascripts/tourToggle.js
@@ -3,10 +3,26 @@ $( document ).ready(function() {
     var toggleOne = $('.tour-toggle-one')
         toggleTwo = $('.tour-toggle-two')
         sectionOne = $('.tour-one')
-        sectionTwo = $('.tour-two');
+        sectionTwo = $('.tour-two')
+        optionOne = $("#ticket-option-one")
+        optionTwo = $("#ticket-option-two");
+
+    $(optionOne).click( function() {
+      $(toggleOne).removeClass('tour-toggle-hidden')
+      $(toggleTwo).addClass('tour-toggle-hidden')
+      $(sectionTwo).removeClass('section-hidden')
+      $(sectionOne).addClass('section-hidden')
+    });
+
+    $(optionTwo).click( function() {
+      $(toggleTwo).removeClass('tour-toggle-hidden')
+      $(toggleOne).addClass('tour-toggle-hidden')
+      $(sectionTwo).removeClass('section-hidden')
+      $(sectionOne).addClass('section-hidden')
+    });
+
 
     $(toggleTwo).click( function() {
-      console.log('ok');
       $(this).removeClass('tour-toggle-hidden')
       $(toggleOne).addClass('tour-toggle-hidden')
       $(sectionTwo).removeClass('section-hidden')
@@ -15,7 +31,6 @@ $( document ).ready(function() {
 
 
     $(toggleOne).click( function() {
-      console.log('ok');
       $(this).removeClass('tour-toggle-hidden')
       $(toggleTwo).addClass('tour-toggle-hidden')
       $(sectionOne).removeClass('section-hidden')
